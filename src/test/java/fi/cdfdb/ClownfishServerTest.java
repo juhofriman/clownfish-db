1188689e777bd74f563f0b1154ad63ff2c86dc25
Juho Friman <juho.t.friman@gmail.com>
2021-01-29 19:16:41 +0200

    Initial commit

---
+++ src/test/java/fi/cdfdb/ClownfishServerTest.java
@@ -0,0 +1,11 @@
+package fi.cdfdb;
+
+import org.junit.jupiter.api.Test;
+
+public class ClownfishServerTest {
+
+   @Test
+   public void testCreateServer() {
+      new ClownfishServer();
+   }
+}
